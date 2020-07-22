5aaebfd5e2609332c2c45597058ccd5ff49955c8
FloreErin <floreerin@gmail.com>
2020-07-22 16:35:42 +0900

    좋아요 버튼 기능 추가, DetailView UI 수정

--- app/src/main/java/com/flore/instagramclone/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/flore/instagramclone/navigation/DetailViewFragment.kt
@@ -10,12 +10,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.flore.instagramclone.R
 import com.flore.instagramclone.navigation.model.ContentDTO
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_detail.view.*
 import kotlinx.android.synthetic.main.item_detail.view.*
 
 class DetailViewFragment : Fragment() {
     var firestore: FirebaseFirestore? = null // DB 내용에 담긴 데이터를 가져오기 위해 사용
+    var uid: String? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,6 +25,7 @@ class DetailViewFragment : Fragment() {
     ): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail, container, false)
         firestore = FirebaseFirestore.getInstance()
+        uid = FirebaseAuth.getInstance().currentUser?.uid // 유저 ID
 
         view.detailviewfragment_recycleview.adapter = DetailViewRecyclerViewAdapter()
         view.detailviewfragment_recycleview.layoutManager = LinearLayoutManager(activity)
@@ -73,13 +76,42 @@ class DetailViewFragment : Fragment() {
             // 이미지 설명
             viewHolder.detailviewitem_explain_textview.text = contentDTOs!![position].explain
 
-            // 좋아요 표시
+            // 좋아요 내용 표시
             viewHolder.detailviewitem_favoritecounter_textview.text =
                 "Likes " + contentDTOs!![position].favoriteCount
 
-            // 유저 프로필 이미지 로딩 (Glide)
-            Glide.with(holder.itemView.context).load(contentDTOs!![position].imageUrl)
-                .into(viewHolder.detailviewitem_profile_image)
+            // 유저 프로필 이미지 로딩 (Glide) (수정 필요)
+//            Glide.with(holder.itemView.context).load(contentDTOs!![position].imageUrl)
+//                .into(viewHolder.detailviewitem_profile_image)
+
+            // 좋아요 버튼 클릭 이벤트
+            viewHolder.detailviewitem_favorite_imageview.setOnClickListener {
+                favoriteEvent(position)
+            }
+
+            if (contentDTOs!![position].favorites.containsKey(uid)) {
+                viewHolder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite)
+            } else {
+                viewHolder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite_border)
+            }
+        }
+
+        fun favoriteEvent(position: Int) { // 좋아요 표시 이벤트
+            var tsDoc = firestore?.collection("images")?.document(contentUidList[position])
+            firestore?.runTransaction { transaction ->
+
+                var contentDTO = transaction.get(tsDoc!!)
+                    .toObject(ContentDTO::class.java) // 파이어베이스에 있는 content 데이터를 ContentDTO로 케스팅
+
+                if (contentDTO!!.favorites.containsKey(uid)) { // 좋아요 버튼이 이미 클릭 되어 있을 경우
+                    contentDTO?.favoriteCount = contentDTO?.favoriteCount!! - 1
+                    contentDTO?.favorites.remove(uid) // uid 값 삭제
+                } else {
+                    contentDTO?.favoriteCount = contentDTO?.favoriteCount!! + 1
+                    contentDTO.favorites[uid!!] = true // uid 값 hashMap에 리스트로 저장
+                }
+                transaction.set(tsDoc, contentDTO) // 트렌젝션을 종료하고 변경된 값을 온라인에서도 변경
+            }
         }
 
     }
