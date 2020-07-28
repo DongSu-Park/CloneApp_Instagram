310cc2931b3bd93fe5440f368faeb4d7283f5acf
FloreErin <floreerin@gmail.com>
2020-07-28 16:47:40 +0900

    comment 엑티비티 추가, 유저 프로필 이미지 수정

--- app/src/main/java/com/flore/instagramclone/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/flore/instagramclone/navigation/DetailViewFragment.kt
@@ -1,5 +1,6 @@
 package com.flore.instagramclone.navigation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,11 +9,13 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.flore.instagramclone.R
 import com.flore.instagramclone.navigation.model.ContentDTO
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_detail.view.*
+import kotlinx.android.synthetic.main.item_comment.view.*
 import kotlinx.android.synthetic.main.item_detail.view.*
 
 class DetailViewFragment : Fragment() {
@@ -83,9 +86,17 @@ class DetailViewFragment : Fragment() {
             viewHolder.detailviewitem_favoritecounter_textview.text =
                 "Likes " + contentDTOs!![position].favoriteCount
 
-            // 유저 프로필 이미지 로딩 (Glide) (수정 필요)
-//            Glide.with(holder.itemView.context).load(contentDTOs!![position].imageUrl)
-//                .into(viewHolder.detailviewitem_profile_image)
+            // 유저 프로필 이미지 로딩 (Glide)
+            FirebaseFirestore.getInstance()
+                .collection("profileImages")
+                .document(contentDTOs[position].uid!!)
+                .get()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful){
+                        var url = task.result!!["image"]
+                        Glide.with(holder.itemView.context).load(url).apply(RequestOptions().circleCrop()).into(viewHolder.detailviewitem_profile_image)
+                    }
+                }
 
             // 좋아요 버튼 클릭 이벤트
             viewHolder.detailviewitem_favorite_imageview.setOnClickListener {
@@ -108,6 +119,13 @@ class DetailViewFragment : Fragment() {
                 fragment.arguments = bundle
                 activity?.supportFragmentManager?.beginTransaction()
                     ?.replace(R.id.content_main, fragment)?.commit() // 프레그먼트를 UserFragment로 변경
+            }
+
+            // 댓글 클릭 이벤트
+            viewHolder.detailviewitem_comment_imageview.setOnClickListener { v ->
+                var intent = Intent(v.context, CommentActivity::class.java)
+                intent.putExtra("contentUid", contentUidList[position])
+                startActivity(intent)
             }
         }
 
